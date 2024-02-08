12d9a2b3b03d872aa2bf3636962eaed6251a596c
Marco Brunetti <151136910+marcocodd@users.noreply.github.com>
2024-02-08 16:47:46 +0100

    punto 1 e 2

--- src/_tests_/App.test.js
+++ src/_tests_/App.test.js
@@ -1,23 +1,23 @@
 import {render, screen} from '@testing-library/react';
-import App from '../App'
+import App from "../App";
+import arrayBooks from "../data/fantasy.json";
 
-describe ('General mounting compononents',() =>{
+describe("General mounting compononents", () => {
+ it("mounting Welcome component", () => {
+  render(<App />);
 
-it ('mounting Welcome component', ()=>{
+  const alertWelcome = screen.getByRole("alert");
+  expect(alertWelcome).toBeInTheDocument();
+ });
 
-    render(<App/>)
-
-    const alertWelcome = screen.getByRole('alert')
-    expect (alertWelcome).toBeInTheDocument()
-})
-
-
-
-
-
-
-
-
-})
+ it("mounting cards number equal to the jasonBooks", async () => {
+  render(<App />);
+  await (() => {
+   const bookCards = screen.queryAllByTestId("book-card");
+   console.log("numero cards", bookCards.length);
+   expect(bookCards.length).toEqual(arrayBooks.length);
+  });
+ });
+});
 
 
--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -9,27 +9,28 @@ class SingleBook extends Component {
 
   render() {
     return (
-      <>
-        <Card
-          // onClick={() => this.setState({ selected: !this.state.selected })}
-          onClick={() => this.props.changeSelectedBook(this.props.book.asin)}
-          style={{
-            border:
-              this.props.selectedBook === this.props.book.asin
-                ? '3px solid red'
-                : 'none',
-          }}
-        >
-          <Card.Img variant="top" src={this.props.book.img} />
-          <Card.Body>
-            <Card.Title style={{ color: 'black' }}>
-              {this.props.book.title}
-            </Card.Title>
-          </Card.Body>
-        </Card>
-        {/* {this.state.selected && <CommentArea asin={this.props.book.asin} />} */}
-      </>
-    )
+     <>
+      <Card
+       data-testid="book-card"
+       // onClick={() => this.setState({ selected: !this.state.selected })}
+       onClick={() => this.props.changeSelectedBook(this.props.book.asin)}
+       style={{
+        border:
+         this.props.selectedBook === this.props.book.asin
+          ? "3px solid red"
+          : "none",
+       }}
+      >
+       <Card.Img variant="top" src={this.props.book.img} />
+       <Card.Body>
+        <Card.Title style={{ color: "black" }}>
+         {this.props.book.title}
+        </Card.Title>
+       </Card.Body>
+      </Card>
+      {/* {this.state.selected && <CommentArea asin={this.props.book.asin} />} */}
+     </>
+    );
   }
 }
 
